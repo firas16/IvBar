a4e6d41d35b033d86119ea305ec864d063c6119f
firas <firas.fbs.1989@gmail.com>
2018-03-29 18:16:20 +0200

    IvBar divide files

--- main.py
+++ main.py
@@ -30,21 +30,35 @@ tree._setroot(root)
 version = SubElement(root, 'version')
 version.text = '3.0.1'
 
+max_xml_size = 200000
+
 ## Creation of the root's subelement named "careContacts"
 careContacts = SubElement(root, 'careContacts')
 if(source == "DCIR"):
 	data_dcir = pd.read_table(conf["path_dcir"], sep = ';', dtype = conf["columns_types_dcir"])
 	data_dcir["careContactId"] = pd.Series(np.arange(len(data_dcir.index)))
-	data_dcir.apply(lambda x: tree_generator_DCIR(records, x, careContacts), axis = 1 )
+	for i in Range(0, size+1):
+		begin = i*max_xml_size
+		end = Min((i+1)*max_xml_size, len(data_dcir))
+		data_dcir[begin:end].apply(lambda x: tree_generator_DCIR(records, x, careContacts), axis = 1)
+		tree.write(conf["output_path"]+i, encoding = 'UTF-8', xml_declaration = 'True')
 elif (source == "PMSI"):
 	data_sejours = pd.read_table(conf["path_sejours"], sep = ';', dtype = conf["columns_types_sej"]).head(5)
 	data_das = pd.read_table(conf["path_das"], sep = ';')
 	data_ccam = pd.read_table(conf["path_ccam"], sep = ';')
-	data_sejours.apply(lambda x: tree_generator_PMSI(records, x, careContacts, "", data_das, data_ccam), axis = 1 )
+	for i in Range(0, size+1):
+		begin = i*max_xml_size
+		end = Min((i+1)*max_xml_size, len(data_dcir))
+		data_sejours[begin:end].apply(lambda x: tree_generator_PMSI(records, x, careContacts, "", data_das, data_ccam), axis = 1 )
+		tree.write(conf["output_path"]+i, encoding = 'UTF-8', xml_declaration = 'True')
 elif (source == "ACE"):
 	data_ace = pd.read_table(conf["path_ace"], sep = ';', dtype = conf["columns_types_ace"])
 	data_ace["careContactId"] = pd.Series(np.arange(len(data_ace.index)))
-	data_ace.apply(lambda x: tree_generator_ACE(records, x, careContacts), axis = 1 )
+	for i in Range(0, size+1):
+		begin = i*max_xml_size
+		end = Min((i+1)*max_xml_size, len(data_dcir))
+		data_ace[begin:end].apply(lambda x: tree_generator_ACE(records, x, careContacts), axis = 1 )
+		tree.write(conf["output_path"]+i, encoding = 'UTF-8', xml_declaration = 'True')
 
 #Save XML
-tree.write(conf["output_path"], encoding = 'UTF-8', xml_declaration = 'True')
+#tree.write(conf["output_path"], encoding = 'UTF-8', xml_declaration = 'True')
